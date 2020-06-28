80ccf4511d02f446cf38e2b2dace85d0b1028020
Pedro Medina <medinamarquezp@gmail.com>
2020-06-29 00:39:41 +0200

    Refactoring config files

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const env = process.env.NODE_ENV || "development";
-const configFileName = `${env}.config.json`;
+const configFileName = `${env}.config.ts`;
 const config = require(`./${configFileName}`);
 
 export default config;
