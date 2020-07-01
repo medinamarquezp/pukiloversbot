c55985d4023390a6d82a1d7919830e26d8fda3a1
Pedro Medina <medinamarquezp@gmail.com>
2020-07-01 21:11:41 +0200

    Send token by query params

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
     res.status(200).json({ status: 200, message: 'Server Up and running' })
 })
 
-app.get('/publish', async (req, res) => {
-    const { token } = req.headers
+app.get('/publish/:token?', async (req, res) => {
+    const token = req.query.token || req.headers.token
     if (token && token === config.http_token){
         await publish()
         return res.status(200).json({ status: 200, message: "Content has been published successfully"  })
