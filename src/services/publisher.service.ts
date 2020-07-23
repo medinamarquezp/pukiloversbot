57079fabcffe29f868250337b188abac8b6fb034
Pedro Medina <medinamarquezp@gmail.com>
2020-07-23 19:25:09 +0200

    Refactor and fix of save rejected image action

--- src/services/publisher.service.ts
+++ src/services/publisher.service.ts
@@ -6,7 +6,7 @@ import { serverTimestamp } from '../services/db.service'
 import randomArrayElement from '../services/random.service'
 import getProducerInstance from '../producers/producers.factory'
 import IProducer, { IImageObject } from '../producers/IProducer'
-import { save, isPublishedOrRejected, IMedia, mediaStatus } from '../repositories/media.repository'
+import { save, isPublishedOrRejected, mediaStatus } from '../repositories/media.repository'
 
 export const getImageToPublish = async (randomTerm: string, producerInstance: IProducer): Promise<IImageObject> => {
     let mediaObject = { id: '', imageURL: '' }
@@ -20,13 +20,7 @@ export const getImageToPublish = async (randomTerm: string, producerInstance: IP
             const validImage = await isValidImage(media.imageURL)
             console.log('Checking if is a valid image: ', validImage)
             if (!validImage) {
-                await rejectIfInvalidImage({
-                    producer: producerInstance.getType(),
-                    producerID: media.id.toString(),
-                    url: media.imageURL,
-                    status: mediaStatus.rejected,
-                    createdAt: serverTimestamp
-                })
+                await saveImage(media, producerInstance, mediaStatus.rejected)
                 console.log('Image rejected, getting a new image')
                 continue
             } else {
@@ -43,20 +37,12 @@ export const isValidImage = async(url: string): Promise<boolean> => {
     return await new Recognition().validateImageRecognition(url)
 }
 
-export const rejectIfInvalidImage = async (rejectedMedia: IMedia): Promise<boolean> => {
-    if (!isValidImage(rejectedMedia.url)) {
-        await save('media', rejectedMedia.producer, rejectedMedia) 
-        return true
-    }
-    return false
-}
-
-export const saveIfValidImage = async (media: IImageObject, producerInstance: IProducer): Promise<boolean> => {
+export const saveImage = async (media: IImageObject, producerInstance: IProducer, status: mediaStatus): Promise<boolean> => {
     await save('media', producerInstance.getType(), {
         producer: producerInstance.getType(),
         producerID: media.id.toString(),
         url: media.imageURL,
-        status: mediaStatus.published,
+        status: status,
         createdAt: serverTimestamp
     })
     return true
@@ -64,14 +50,14 @@ export const saveIfValidImage = async (media: IImageObject, producerInstance: IP
 
 export const publish = async () => {
     const producerInstance = getProducerInstance()
-    console.log('Getting a producer instance', producerInstance)
+    console.log('Getting a producer instance: ', producerInstance.getType)
     const randomTerm =  randomArrayElement(config.terms)
-    console.log('Getting a random term and content', randomTerm)
+    console.log('Getting a random term and content: ', randomTerm)
     const content = getContent(randomTerm, producerInstance.getType())
     console.log('Getting image to publish')
     const imageToPublish =  await getImageToPublish(randomTerm, producerInstance)
     console.log('Publishing on SSMM')
     await new TweetBot().tweetMedia(imageToPublish.imageURL, content)
-    await saveIfValidImage(imageToPublish, producerInstance)
+    await saveImage(imageToPublish, producerInstance, mediaStatus.published)
     console.log('Published image saved on DB')
 }
